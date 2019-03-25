1f51e9e45dcfaada8657196ea7537dc0e687c5b6
liuganghuan <admin@arloor.com>
2019-03-25 21:39:06 +0800

    Update ddnspod.sh

--- ddnspod.sh
+++ ddnspod.sh
@@ -17,6 +17,8 @@ case $(uname) in
         if [ "x${extip}" = "x" ]; then
 	        extip=$(ip -o -4 addr list | grep -Ev '\s(docker|lo)' | awk '{print $4}' | cut -d/ -f1 )
         fi
+	# 从360的api获取nat的真实公网ip
+	extip=$(wget -qO- http://ip.360.cn/IPShare/info |grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}")
         echo $extip
     }
     ;;
